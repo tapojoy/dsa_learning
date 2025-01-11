cabb1c618967536fba4bb2076a0567ebb9b4510f
keporb <kuantumkomputing@protonmail.com>
2025-01-11 20:56:03 +0530

    Update merge_sort.py
    Add merge_sort routine

--- merge_sort.py
+++ merge_sort.py
@@ -1,34 +1,54 @@
-def merge(A,p,q,r):
-    k = p
-    l = r
-    L = A[p:q+1]
-    R = A[q+1:r+1]
-    l_len = q + 1 - p # len(L)
-    r_len = r - q # len(R)
+def merge(A, p, q, r, lvl, verbose=False):
+    L = A[p:q]
+    R = A[q:r]
+    l_len = q - p
+    r_len = r - q
     i = 0
     j = 0
-    print(" i = " + str(i) + ", j = " + str(j) + ", k = " + str(k) + ", A = " + str(A))
-    while k <= l and i<l_len and j<r_len:
+    if verbose: print(" > > merge routine, lvl = " + str(lvl) + ",")
+    if verbose: print(" > > p = " + str(p) + ", q = " + str(q) + ", r = " + str(r))
+    if verbose: print(" > > L = " + str(L) + ", R = " + str(R))
+    while p <= r and i<l_len and j<r_len:
         if L[i] <= R[j]:
-            A[k] = L[i]
+            A[p] = L[i]
             i = i + 1
-            print(" i = " + str(i) + ", j = " + str(j) + ", k = " + str(k) + ", A = " + str(A))
+            if verbose: print(" > > i = " + str(i) + ", j = " + str(j) + ", p = " + str(p) + ", A = " + str(A))
         else:
-            A[k] = R[j]
+            A[p] = R[j]
             j = j + 1
-            print(" i = " + str(i) + ", j = " + str(j) + ", k = " + str(k) + ", A = " + str(A))
-        k = k + 1
-    if k!=l:
+            if verbose: print(" > > i = " + str(i) + ", j = " + str(j) + ", p = " + str(p) + ", A = " + str(A))
+        p = p + 1
+    if p!=r:
         if j == r_len:
             while i<l_len:
-                A[k] = L[i]
-                k += 1
+                A[p] = L[i]
+                p += 1
                 i += 1
-                print(" i = " + str(i) + ", j = " + str(j) + ", k = " + str(k) + ", A = " + str(A))
+                if verbose: print(" > > i = " + str(i) + ", j = " + str(j) + ", p = " + str(p) + ", A = " + str(A))
         elif i == l_len:
             while j<r_len:
-                A[k] = R[j]
-                k += 1
+                A[p] = R[j]
+                p += 1
                 j += 1
-                print(" i = " + str(i) + ", j = " + str(j) + ", k = " + str(k) + ", A = " + str(A))
+                if verbose: print(" > > i = " + str(i) + ", j = " + str(j) + ", p = " + str(p) + ", A = " + str(A))
     return A
+
+def merge_sort(array, p, r, lvl=0, verbose=False):
+    if not isinstance(array,list):
+        return None
+    lvl += 1
+    l = r - p
+    if l <= 1:
+        if verbose: print(" > base condition, p = " + str(p) + ", r = " + str(r) + ", A[p] = " + str(array[p]))
+        return array
+    else:
+        q = p + int(l / 2)
+
+        if verbose: print("lvl = " + str(lvl) + ", induction, p = " + str(p) + ", q = " + str(q) + ", r = " + str(r))
+        array = merge_sort(array, p, q, lvl, verbose)
+
+        if verbose: print("lvl = " + str(lvl) + ", induction, p = " + str(p) + ", q = " + str(q) + ", r = " + str(r))
+        array = merge_sort(array, q, r, lvl, verbose)
+
+        array = merge(array, p, q, r, lvl, verbose)
+        return array
