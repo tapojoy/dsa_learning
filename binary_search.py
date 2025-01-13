d7f17f1eb45800e0b5947516651a0c20d1f95ce4
keporb <kuantumkomputing@protonmail.com>
2025-01-13 21:22:17 +0530

    Update binary_search.py
    Implement using left and right pointers, and return index if value found

--- binary_search.py
+++ binary_search.py
@@ -1,23 +1,30 @@
-def binary_search(arr, v, lvl=0, verbose=False):
+def binary_search(arr, v, left, right, lvl=0, verbose=False):
     lvl += 1
-    l = len(arr)
-    q = int(l/2)
-    res = False
+    mid = int((left+right)/2)
     if verbose:
+        l = len(arr)
         indices = []
         for i in range(l): indices.append(i)
-        print("    arr = " + str(arr))
+        print("\n    arr = " + str(arr))
         print("indices = " + str(indices))
-        print("lvl = " + str(lvl) + ", l = " + str(l) + ", q = " + str(q) + ", arr[q] = " + str(arr[q]) + ", v = " + str(v) + " | ")
-    if l == 1:
-        if v == arr[q]:
-            res = True
-        return res
+        print(
+            "lvl = " + str(lvl) +
+            ", (left, right) = (" + str(left) + ", " + str(right) + ")" +
+            ", mid = " + str(mid) +
+            ", arr[mid] = " + str(arr[mid]) +
+            ", v = " + str(v)
+        )
+    if left > right:
+        if verbose: print("left > right, value not found")
+        return None
+    elif v == arr[mid]:
+        if verbose: print("value found at index : " + str(mid))
+        return mid
+    elif v < arr[mid]:
+        right = mid - 1
+        if verbose: print("right = mid - 1 = " + str(right))
+        return binary_search(arr, v, left, right, lvl, verbose)
     else:
-        if v == arr[q]:
-            res = True
-        elif v < arr[q]:
-            res = binary_search(arr[:q], v, lvl, verbose)
-        else:
-            res = binary_search(arr[q:l], v, lvl, verbose)
-        return res
+        left = mid + 1
+        if verbose: print("left = mid + 1 = " + str(left))
+        return binary_search(arr, v, left, right, lvl, verbose)
