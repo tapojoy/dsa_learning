bdb3e7446e0c1b7b56e6c69128c5d5ff84283975
keporb <kuantumkomputing@protonmail.com>
2025-02-07 23:29:47 +0530

    Update BinarySearchTrees.py
    Add tree_search, tree_search_iterative, tree_minimum and tree_maximum procedures

--- BinarySearchTrees.py
+++ BinarySearchTrees.py
@@ -24,3 +24,34 @@ def postorder_tree_walk(x):
     postorder_tree_walk(x.right)
     print(x.key)
 
+def tree_search(x, key):
+    if not x:
+        return None
+    if key == x.key:
+        return x
+    if key < x.key:
+        return tree_search(x.left, key)
+    else:
+        return tree_search(x.right, key)
+
+def tree_search_iterative(x, key):
+    while True:
+        if not x:
+            return None
+        if key == x.key:
+            return x
+        if key < x.key:
+            x = x.left
+        else:
+            x = x.right
+
+def tree_minimum(x):
+    while x.left is not None:
+        x = x.left
+    return x
+
+def tree_maximum(x):
+    while x.right is not None:
+        x = x.right
+    return x
+
