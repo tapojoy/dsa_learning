06092734ba679dfcc5f47d68e31d2dccb9a5b66e
keporb <kuantumkomputing@protonmail.com>
2025-02-10 14:43:56 +0530

    Update RedBlackTrees.py
    Add rb_insert and rb_insert_fixup procedures to perform the insertion operation in RedBlackTrees

--- RedBlackTrees.py
+++ RedBlackTrees.py
@@ -38,7 +38,7 @@ class RedBlackTrees:
         x.right = y.left
         if y.left is not self.NIL:
             y.left.p = x
-        x.p = y.p
+        y.p = x.p
         if x.p is self.NIL:
             self.root = y
         elif x is x.p.left:
@@ -48,7 +48,7 @@ class RedBlackTrees:
         y.left = x
         x.p = y
 
-    def rotate_right(self, y):
+    def right_rotate(self, y):
         assert isinstance(y, RedBlackTreeNode), 'incorrect input type'
         assert y.left is not self.NIL, 'NIL left child, cannot rotate'
         x = y.left
@@ -64,4 +64,56 @@ class RedBlackTrees:
             y.p.right = x
         x.right = y
         y.p = x
+
+    def rb_insert(self, key):
+        if self.root.is_none():
+            self.root.set_key(key)
+            return True
+        new_node = RedBlackTreeNode(key)
+        x = self.root
+        while True:
+            y = x
+            x = x.left if key < x.key else x.right
+            if x is self.NIL:
+                new_node.p = y
+                if key < y.key: y.left = new_node
+                else: y.right = new_node
+                new_node.left = self.NIL
+                new_node.right = self.NIL
+                new_node.set_color_red()
+                self.rb_insert_fixup(new_node)
+                break
+
+    def rb_insert_fixup(self, node):
+        while node.p.is_color_red():
+            if node.p is node.p.p.left:
+                y = node.p.p.right
+                if y.is_color_red():
+                    node.p.set_color_black()
+                    y.set_color_black()
+                    node.p.p.set_color_red()
+                    node = node.p.p
+                else: # elif y.is_color_black()
+                    if node is node.p.right:
+                        node = node.p
+                        self.left_rotate(node)
+                    node.p.set_color_black()
+                    node.p.p.set_color_red()
+                    self.right_rotate(node.p.p)
+            else: # elif node.p is node.p.p.right:
+                y = node.p.p.left
+                if y.is_color_red():
+                    node.p.set_color_black()
+                    y.set_color_black()
+                    node.p.p.set_color_red()
+                    node = node.p.p
+                else: # elif y.is_color_black()
+                    if node is node.p.left:
+                        node = node.p
+                        self.right_rotate(node)
+                    node.p.set_color_black()
+                    node.p.p.set_color_red()
+                    self.left_rotate(node.p.p)
+        self.root.set_color_black()
+
 
