54003a7efb9732585f014284d15c4a151170af29
keporb <kuantumkomputing@protonmail.com>
2025-02-22 21:56:46 +0530

    Update BreadthFirstSearch.py
    update procedure to get path in a breadth first tree and add procedure to get the diameter of a breadth first tree

--- BreadthFirstSearch.py
+++ BreadthFirstSearch.py
@@ -1,3 +1,5 @@
+import random
+
 class Vertex:
     def __init__(self, key=None):
         self.key = key
@@ -31,14 +33,44 @@ def bfs(graph, source):
                 vertex_v.pi = vertex_u
                 queue.append(vertex_v)
 
-def get_path(graph, source, vertex):
+def get_path(source, vertex):
     assert isinstance(vertex, Vertex), 'invalid vertex'
     assert isinstance(source, Vertex), 'invalid source'
-    assert source in graph
-    assert vertex in graph
-    path = str(vertex.key)
+    path = [vertex]
     while True:
         vertex = vertex.pi
-        if vertex is source: return path + ' -> ' + str(source.key)
-        elif vertex is None: return path + ' -> ' + str('discontinuity')
-        path += ' -> ' + str(vertex.key)
+        if vertex is source: break
+        elif vertex is None: return None
+        path.append(vertex)
+    path.append(source)
+    return path
+
+def get_path_str(source, vertex):
+    path = get_path(source, vertex)
+    if path:
+        path_str = str(path.pop(0).key)
+        while path:
+            path_str += ' -> ' + str(path.pop(0).key)
+        return path_str
+    return ('no path found from vertex:' +
+            str(vertex.key) +
+            ' to source:' +
+            str(source.key))
+
+def get_diameter(graph, source):
+    assert isinstance(graph, list)
+    assert isinstance(source, Vertex)
+    assert source in graph
+    graph = graph.copy()
+    graph.remove(source)
+    diameter = 0
+    while graph:
+        random_vertex = random.randint(0, len(graph)-1)
+        path = get_path(source, graph[random_vertex])
+        if not path: break
+        if len(path)-1 > diameter: diameter = len(path)-1
+        for vertex in path:
+            if vertex in graph:
+                graph.remove(vertex)
+    return diameter
+
