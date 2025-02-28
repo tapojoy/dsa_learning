d5e4698350469fe0cb40b1cbfa81447814f97e5f
keporb <kuantumkomputing@protonmail.com>
2025-02-28 17:59:52 +0530

    Update DepthFirstSearch.py
    Remove color attribute and identify types of edges using discovery and finish time attributes

--- DepthFirstSearch.py
+++ DepthFirstSearch.py
@@ -2,43 +2,20 @@ class Vertex:
     def __init__(self, key=None):
         self.key = key
         self.adj = []
-        self.d = 0
-        self.f = 0
-        self.color = None
+        self.d = None
+        self.f = None
         self.pi = None
-
-    def set_color_white(self):
-        self.color = 'white'
-
-    def is_color_white(self):
-        if self.color == 'white': return True
-        return False
-
-    def set_color_gray(self):
-        self.color = 'gray'
-
-    def is_color_gray(self):
-        if self.color == 'gray': return True
-        return False
-
-    def set_color_black(self):
-        self.color = 'black'
-
-    def is_color_black(self):
-        if self.color == 'black': return True
-        return False
 
 
 def dfs(graph):
     for vertex in graph:
-        vertex.d = 0
-        vertex.f = 0
+        vertex.d = None
+        vertex.f = None
         vertex.pi = None
-        vertex.set_color_white()
     time = 0
     edges = []
     for vertex in graph:
-        if vertex.is_color_white():
+        if not vertex.d:
             time, edges = dfs_visit(vertex, time, edges)
     return edges
 
@@ -47,18 +24,16 @@ def dfs_visit(vertex_u, time, edges):
     vertex_u.d = time
     vertex_u.set_color_gray()
     for vertex_v in vertex_u.adj:
-        if vertex_v.is_color_white():
+        if not vertex_v.d:
             vertex_v.pi = vertex_u
             edges.append((vertex_u, vertex_v, 'T'))
             time, edges = dfs_visit(vertex_v, time, edges)
-        elif vertex_v.is_color_gray():
+        elif not vertex_v.f:
             edges.append((vertex_u, vertex_v, 'B'))
-        elif vertex_v.is_color_black():
-            if vertex_u.d < vertex_v.d:
-                edges.append((vertex_u, vertex_v, 'F'))
-            else:
-                edges.append((vertex_u, vertex_v, 'C'))
+        elif vertex_u.d < vertex_v.d:
+            edges.append((vertex_u, vertex_v, 'F'))
+        else:
+            edges.append((vertex_u, vertex_v, 'C'))
     time += 1
     vertex_u.f = time
-    vertex_u.set_color_black()
     return time, edges
