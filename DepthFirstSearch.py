d300157f108441290ec4a1d3f721631c18dacb73
keporb <kuantumkomputing@protonmail.com>
2025-02-28 19:07:47 +0530

    Update DepthFirstSearch.py
    Implement DFS in dfs_2 procedure without recursion using a stack and correct the implementation of commit 155e1e8 by returning the topologically sorted graph

--- DepthFirstSearch.py
+++ DepthFirstSearch.py
@@ -20,7 +20,7 @@ def dfs(graph):
             time, edges, topologically_sorted_graph = dfs_visit(
                 vertex, time, edges, topologically_sorted_graph
             )
-    return edges
+    return edges, topologically_sorted_graph
 
 
 def dfs_visit(vertex_u, time, edges, topologically_sorted_graph):
@@ -43,3 +43,40 @@ def dfs_visit(vertex_u, time, edges, topologically_sorted_graph):
     vertex_u.f = time
     topologically_sorted_graph.insert(0, vertex_u)
     return time, edges, topologically_sorted_graph
+
+
+def dfs_2(graph):
+    for vertex in graph:
+        vertex.d = None
+        vertex.f = None
+        vertex.pi = None
+    time = 0
+    for vertex in graph:
+        if not vertex.d:
+            time = dfs_visit_2(vertex, time)
+
+
+def dfs_visit_2(node_u, time):
+    time += 1
+    node_u.d = time
+    dfs_stack = [node_u]
+    while dfs_stack:
+        next_node = None
+        adj = node_u.adj.copy()
+        adj.reverse()
+        for node_v in adj:
+            if not node_v.d:
+                next_node = node_v
+                next_node.pi = node_u
+        if next_node:
+            node_u = next_node
+            time += 1
+            node_u.d = time
+            dfs_stack.append(node_u)
+        else:
+            node_v = dfs_stack.pop()
+            time += 1
+            node_v.f = time
+            node_u = node_v.pi
+    return time
+
