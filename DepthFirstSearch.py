155e1e8acff65096664e1dfb20e7937fd70e4d05
keporb <kuantumkomputing@protonmail.com>
2025-02-28 18:16:43 +0530

    Update DepthFirstSearch.py
    Update DFS procedure to return the vertices of the graph in a topologically sorted list and fix dfs_visit procedure to correctly follow changes in commit d5e4698

--- DepthFirstSearch.py
+++ DepthFirstSearch.py
@@ -14,20 +14,25 @@ def dfs(graph):
         vertex.pi = None
     time = 0
     edges = []
+    topologically_sorted_graph = []
     for vertex in graph:
         if not vertex.d:
-            time, edges = dfs_visit(vertex, time, edges)
+            time, edges, topologically_sorted_graph = dfs_visit(
+                vertex, time, edges, topologically_sorted_graph
+            )
     return edges
 
-def dfs_visit(vertex_u, time, edges):
+
+def dfs_visit(vertex_u, time, edges, topologically_sorted_graph):
     time += 1
     vertex_u.d = time
-    vertex_u.set_color_gray()
     for vertex_v in vertex_u.adj:
         if not vertex_v.d:
             vertex_v.pi = vertex_u
             edges.append((vertex_u, vertex_v, 'T'))
-            time, edges = dfs_visit(vertex_v, time, edges)
+            time, edges, topologically_sorted_graph = dfs_visit(
+                vertex_v, time, edges, topologically_sorted_graph
+            )
         elif not vertex_v.f:
             edges.append((vertex_u, vertex_v, 'B'))
         elif vertex_u.d < vertex_v.d:
@@ -36,4 +41,5 @@ def dfs_visit(vertex_u, time, edges):
             edges.append((vertex_u, vertex_v, 'C'))
     time += 1
     vertex_u.f = time
-    return time, edges
+    topologically_sorted_graph.insert(0, vertex_u)
+    return time, edges, topologically_sorted_graph
